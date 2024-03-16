5dd550b54e085a6d6870dfb2fa8b06736e267c93
erfan <erfanghezi5050@gmail.com>
2024-03-16 21:49:42 +0330

    update file

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,12 @@ module.exports = {
         "pink" : "#9C27B0",
         "dark" : "#282830"
       },
-      screens:{
-        xl:{max:"1200px"},
-        lg:{max:"992px"},
-        md:{max:"768px"},
-        sm:{max:"576px"},
-      }
+    },
+    screens:{
+      xl:{max:"1200px"},
+      lg:{max:"992px"},
+      md:{max:"768px"},
+      sm:{max:"576px"},
     },
   },
   plugins: [],
